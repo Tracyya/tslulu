7c847fb81a48f7941df69583d5c1aaa8115c618f
yc08230823 <yc08230823@163.163.com>
2021-10-12 21:19:07 +0800

    实现列表显示

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,4 +1,4 @@
-import React , { useState } from 'react'
+import React , { useState, useEffect } from 'react'
 
 import * as qs from 'qs'
 
@@ -9,23 +9,33 @@ import SearchPanel from './searchPanel';
 import {} from 'utiles'
 
 const apiUrl = process.env.REACT_APP_API_URL;
-
-
-
 const ProjectListScreen = () => {
-    const [param,setParam]=useState({
+    const [ users, setUsers ] = useState([])
+    const [ param, setParam ] = useState({
         name:"",
         personId:""
     })
-    const [list,setList]=useState([]);
-    const [users,setUsers]=useState([])
-
-    return ( 
-        <div>
-            <SearchPanel users={users} param={param} setParam={setParam} />
-            <List list={list} users={users} />
-        </div>
-     );
-}
- 
-export default ProjectListScreen;
+    const [ list, setList ] = useState([])
+    useEffect(() => {
+       fetch(`${apiUrl}/projects`).then(async response => {
+           if(response.ok){
+              setList(await response.json()) 
+           }
+       })
+    }, [param])
+    useEffect(() => {
+        fetch(`${apiUrl}/users`).then(async response => {
+            if(response.ok){
+                setUsers(await response.json()) 
+            }
+        })
+     }, [])
+      return ( 
+            <div>
+                <SearchPanel users={users} param={param} setParam={setParam} />
+                <List list={list} users={users} />
+            </div>
+         );
+    }
+     
+    export default ProjectListScreen;
--- src/screens/project-list/searchPanel.jsx
+++ src/screens/project-list/searchPanel.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
 
+import React,{ useState, useEffect } from 'react'
 const SearchPanel = (props) => {
     let { users , param , setParam } = props
+   
     return ( 
         <div>
             <form action="">
-                <input type="text" value={param.name} onChange={e=>setParam({...param,name:e.target.value})} />
-                <select value={param.personId} onChange={e=>setParam({...param,personId:e.target.value})} >
-                    <option value="">负责人</option>
-                    {
-                        users.map(user=>
-                        <option value={user.id} key={user.id}>
-                            {user.name}
-                        </option>)
-                    }
-                </select>
+                  <input type="text" value={param.name}  onChange={(e)=>setParam({
+                    ...param,
+                    name:e.target.value
+                  })}/>
             </form>
+            <select value={param.personId} onChange={(e)=>setParam({
+                ...param,
+                personId:e.target.value
+            })}>
+                <option value={""}>负责人</option>
+                 {
+                     users.map(user=><option value={user.id} >{user.name}</option>)
+                 }
+            </select>
         </div>
      );
 }
