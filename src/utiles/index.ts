753a294c8b7b37699a805aa92831fe3696a34eb6
Tracyya <tracy225428@163.com>
2021-10-14 14:49:31 +0800

    安装devtool

--- src/utiles/index.ts
+++ src/utiles/index.ts
@@ -1,37 +1,32 @@
-import React,{ useEffect, useState } from 'react';
-
-// 一个！是对他的值取反 ！！是对值取反之后在取反
-export const isFalsy=(value:unknown)=>value === 0? false:!value
-export const cleanObject=(object:object)=>{
-const result={...object}
-Object.keys(result).forEach(key=>{
+import React,{ useEffect, useState } from "react";
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+// 在一个函数里，改变传入的对象本身是不好的
+export const cleanObject = (object: object) => {
+  const result = { ...object };
+  Object.keys(result).forEach((key: string) => {
     //@ts-ignore
-    const value=result[key]
-    
-    if(isFalsy(value)){
-        //@ts-ignore
-        delete result[key]
+    const value = result[key];
+    if (isFalsy(value)) {
+      //@ts-ignore
+      delete result[key];
     }
-})
-return result
-}
-// callback 也需要一个类型 我们给一个函数 ()=>void   代表是不需要返回值
-export const useMount = (callback:()=>void) => {
-    useEffect(() => {
-        callback()
-    }, [])
-}
+  });
+  return result;
+};
 
-///去抖的闭包函数
-export const useDebounce = <V>(value:V, delay?:number)=> {
-  //每次在value变化之后设置一个定时器
-    const [debouncedValue, setDebouncedValue] = useState(value)
-   useEffect(() => {
-     const timeout = setTimeout(() => setDebouncedValue(value), delay);
-     //每次在上一个useEffect处理完运行   
-     return () => {
-            clearTimeout(timeout)
-        }
-    }, [value, delay])
-   return debouncedValue
-} 
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+//使用泛型规范类型
+export const useDebounce = <V>(value: V, delay?: number): any => {
+  // 每次在value变化以后，设置一个定时器
+  const [debounceValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
+};
